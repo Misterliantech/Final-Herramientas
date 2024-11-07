63114ce95c2b75f91b922130c4abea2ad4ef7478
JulianchoXX <julianquinterortiz@gmail.com>
2024-11-06 20:49:38 -0500

    Configuracion

---
+++ src/main/java/database/ConexionDB.java
@@ -0,0 +1,15 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionDB {
+    
+    private static final String url = "jdbc:mysql://localhost:3306/Aerolinea";
+    private static final String user = "Quintero";
+    private static final String password = "clave123";
+    
+    
+} 
+
